b2a587d9aaf9db9ae8488f41d6e3e99c984579b9
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2023-11-01 08:26:50 +0700

    Add program files

--- ProductConsole/Program.cs
+++ ProductConsole/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using productconsole.Services;
+
+namespace productconsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MenuService menuService = new MenuService();
+            menuService.Start();
+        }
+    }
+}
